02ded7e875cadbe08b3296e4ced9ae03da1c136c
calebslade <caleb.slade10@gmail.com>
2024-05-16 16:48:40 -0600

    We now will make all units using no inheritance

--- GCGJ/Assets/Units/Unit.cs
+++ GCGJ/Assets/Units/Unit.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit
+public class Zombie
 {
     int numberOfUnit = 0;
-    int resourcesPerSecond = 0;
-    int costCorpse = 0;
-    int costMana = 0;
-}
-public class Zombie : Unit
-{
-    string resourceType = 'Corpses';
-    costCorpse = 1;
-    costMana = 10;
-    resourcesPerSecond = .2;
+    double resourcesPerSecond = .2;
+    int corpseCost = 1;
+    int manaCost = 10;
+
 }
