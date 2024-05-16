6ce5a7172db90d96ea6ceea41287bcce34ef7f95
calebslade <caleb.slade10@gmail.com>
2024-05-16 16:59:36 -0600

    Added increase zombie

--- GCGJ/Assets/Units/Zombie.cs
+++ GCGJ/Assets/Units/Zombie.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Zombie
 {
     int numberOfUnit = 0;
+
+    int increaseUnit = 1;
     double resourcesPerSecond = .2;
     int corpseCost = 1;
     int manaCost = 10;
+
+    increaseNumberOfUnit()
+    {
+        return numberOfUnit + increaseUnit
+    }
+    
 
 }
