f9f243ccd7ec8e3f7dd17f9e70c2494dd354347d
calebslade <caleb.slade10@gmail.com>
2024-05-16 16:33:40 -0600

    Added unit and zombie classes

---
+++ GCGJ/Assets/Units/Unit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit
+{
+    int numberOfUnit = 0;
+    int resourcesPerSecond = 0;
+    int costCorpse = 0;
+    int costMana = 0;
+}
---
+++ GCGJ/Assets/Units/Zombie.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unit.cs;
+public class Zombie : Unit
+{
+    string resourceType = 'Corpses';
+    costCorpse = 1;
+    costMana = 10;
+    resourcesPerSecond = .2;
+}
+
