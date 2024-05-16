67b819775e3d32829eb636d8d9c9e3ee71574edc
guccigeg <gregshipley19@gmail.com>
2024-05-16 16:33:31 -0600

    Update GameManager.cs

--- GCGJ/Assets/_Scripts/GameManager.cs
+++ GCGJ/Assets/_Scripts/GameManager.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class GameManager : MonoBehaviour
-{
-    [SerializeField] private float health 10.0f;
-    [SerializeField] float death 10.0f;
-    [SerializeField] float life 10.0f;
-    [SerializeField] float gold 10.0f;
+public class GameManager : MonoBehaviour {
+    [SerializeField] private float health = 10.0f;
+    [SerializeField] private float death = 10.0f;
+    private void Update() {
 
+    }
+    public void LoseHealth(float lostHealth) {
+        health -= lostHealth;
+    }
+    public void GainHealth(float gainedHealth) {
+        health += gainedHealth;
+    }
 
-    Debug.log(health, death, life, gold);
+    public void LoseDeath(float lostDeath) {
+        death -= lostDeath;
+    }
+    public void GainDeath(float gainedDeath) {
+        death += gainedDeath;
+    }
 }
